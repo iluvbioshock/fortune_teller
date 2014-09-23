ea1ae1238b4ef3311cc9e868dbca442e947ca227
Krystal Scott <iluvhugs4evr@gmail.com>
2014-09-23 13:40:36 -0700

    first commit

---
+++ app/src/main/java/android/scottk/crystal_ball/predictions.java
@@ -0,0 +1,11 @@
+package android.scottk.crystal_ball;
+
+import android.gesture.Prediction;
+
+/**
+ * Created by Student on 9/19/2014.
+ */
+public class predictions {
+    public static Prediction
+    private Prediction
+}
