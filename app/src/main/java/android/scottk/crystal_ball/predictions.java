da6c43d57a8daaad2c21a26c3088d5ab807ca98e
Krystal Scott <iluvhugs4evr@gmail.com>
2014-09-25 20:26:53 -0700

    new commit milestone 14

--- app/src/main/java/android/scottk/crystal_ball/predictions.java
+++ app/src/main/java/android/scottk/crystal_ball/predictions.java
@@ -5,7 +5,24 @@ import android.gesture.Prediction;
 /**
  * Created by Student on 9/19/2014.
  */
-public class predictions {
-    public static Prediction
-    private Prediction
+public class Predictions {
+    private static Predictions predictions;
+    private String[] answers;
+
+    private Predictions() {
+        answers = new String[] {
+                "your wish will come to pass.",
+                "Your wishes will NEVER come true"
+
+        };
+    }
+    private static Predictions get() {
+        if(predictions == null){
+          predictions = new Predictions();
+        }
+        return predictions;
+    }
+    public String getPrediction() {
+        return answers[0];
+    }
 }
