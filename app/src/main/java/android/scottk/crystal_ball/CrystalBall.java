790e5f3ca4703ec2fc625af3a15b244450dba379
Krystal Scott <iluvhugs4evr@gmail.com>
2014-09-27 00:03:20 -0700

    milestone 21

--- app/src/main/java/android/scottk/crystal_ball/CrystalBall.java
+++ app/src/main/java/android/scottk/crystal_ball/CrystalBall.java
@@ -7,7 +7,9 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class CrystalBall extends Activity {
@@ -23,7 +25,18 @@ public class CrystalBall extends Activity {
     private final SensorEventListener sensorListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+            previousAcceleration = currentAcceleration;
+            currentAcceleration = FloatMath.sqrt(x * x + y * y + z * z);
+            float delta = currentAcceleration - previousAcceleration;
+            acceleration = acceleration * 0.9f + delta;
 
+            if(acceleration > 15) {
+                Toast toast = Toast.makeText(getApplication(), "Device has shaken", Toast.LENGTH_SHORT);
+                toast.show();
+            }
         }
 
         @Override
